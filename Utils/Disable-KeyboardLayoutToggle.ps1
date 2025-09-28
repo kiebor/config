43d1cc072d501e1915457b03b973d6cb46d95a49
Boris K. <46499344+kiebor@users.noreply.github.com>
2025-09-28 13:30:29 +0200

    Create Disable-KeyboardLayoutToggle.ps1

---
+++ Utils/Disable-KeyboardLayoutToggle.ps1
@@ -0,0 +1,3 @@
+reg.exe add "HKCU\Keyboard Layout\Toggle" /v "Language Hotkey" /t REG_SZ /d "3" /f
+reg.exe add "HKCU\Keyboard Layout\Toggle" /v "Hotkey" /t REG_SZ /d "3" /f
+reg.exe add "HKCU\Keyboard Layout\Toggle" /v "Layout Hotkey" /t REG_SZ /d "3" /f 
